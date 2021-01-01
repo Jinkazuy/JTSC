d2c71205a63e1d17f32e37c720e7ab48182e8c6f
Jinkazuy <632931267@qq.com>
2021-01-01 12:37:27 +0800

    202001012020-01-01

---
+++ pages/home/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="page-home">
+		page-home
+	</div>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
+<style lang="stylus" scoped>
+	.page-home {
+		background-color pink;
+	}
+</style>
